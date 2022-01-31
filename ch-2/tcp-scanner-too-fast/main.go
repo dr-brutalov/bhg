41b25b3882bc7d7c55c520cfdc9f172954d54cd4
clay-carper <ccarper2@uwyo.edu>
2022-01-31 13:43:59 -0700

    Worker pool

--- ch-2/tcp-scanner-too-fast/main.go
+++ ch-2/tcp-scanner-too-fast/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"sync"
 )
 
-func main() {
-	for i := 1; i <= 1024; i++ {
-		go func(j int) {
-			address := fmt.Sprintf("scanme.nmap.org:%d", j)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return
-			}
-			conn.Close()
-			fmt.Printf("%d open\n", j)
-		}(i)
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
 	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	var wg sync.WaitGroup
+	for i := 1; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		ports <- i
+	}
+	wg.Wait()
+	close(ports)
 }
